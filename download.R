44eda8679a5522d91c7c09f0082dc09b5561d137
emanuele-guidotti <emanuele.guidotti@hotmail.it>
2021-10-20 09:40:12 +0200

    Update download

--- download.R
+++ download.R
@@ -22,7 +22,8 @@ x <-
       "vacina_descricao_dose" = "character"), 
     col.names = c("date", "ibge", "type"), 
     encoding = "UTF-8", 
-    sep = ";", 
+    sep = ";",
+    header = TRUE,
     tmpdir = getwd(),
     data.table = FALSE,
     verbose = TRUE) %>%
