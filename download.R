3da36a62bdab00e618b1902c716de33b2c07aaa8
Emanuele Guidotti <emanuele.guidotti@unine.ch>
2022-05-07 22:48:36 -0400

    Update download

--- download.R
+++ download.R
@@ -1,29 +1,39 @@
 library(magrittr)
+options(timeout = 0)
 
 # Read state code from argument (e.g. ES for Espirito Santo)
 state <- commandArgs(trailingOnly = TRUE)
 if(length(state) != 1)
   stop("Usage: Rscript download.R <state-code>")
 
-# Web scrape the page to find the latest URL to the data file
-url <- 
-  rvest::read_html("https://opendatasus.saude.gov.br/dataset/covid-19-vacinacao/resource/ef3bd0b8-b605-474b-9ae5-c97390c197a8") %>%
-  rvest::html_nodes(sprintf('a:contains("Dados %s")', state)) %>% 
-  rvest::html_attr('href')
+# Base urls containing the data files
+baseurls <- c(
+  "https://opendatasus.saude.gov.br/dataset/covid-19-vacinacao/resource/5093679f-12c3-4d6b-b7bd-07694de54173",
+  "https://opendatasus.saude.gov.br/dataset/covid-19-vacinacao/resource/10aed154-04c8-4cf4-b78a-8f0fa1bc5af4"
+)
 
-# Increase timeout to 1-hour and download
-options(timeout = 60*60)
-file <- tempfile(tmpdir = getwd())
-download.file(url, destfile = file, quiet = FALSE)
+# Web scrape the pages to find the latest URL to the data file
+urls <- 
+  unname(unlist(lapply(baseurls, function(baseurl){
+    rvest::read_html(baseurl) %>%
+      rvest::html_nodes(sprintf('a:contains("Dados %s")', state)) %>% 
+      rvest::html_attr('href')  
+  })))
+  
 
-# Compute vaccination data
-x <- 
-  # download and read file
+# Compute vaccination data for each file
+x <- lapply(urls, function(url){
+  
+  # download
+  file <- tempfile(tmpdir = getwd())
+  download.file(url, destfile = file, quiet = FALSE)
+  
+  # read file
   data.table::fread(
-    cmd = sprintf("awk --field-separator=';' '{print $28\";\"$8\";\"$29}' %s", file), 
+    cmd = sprintf("awk -F ';' '{print $28\";\"$8\";\"$29}' %s", file), 
     select = c(
-      "vacina_dataaplicacao" = "character", 
-      "paciente_endereco_coibgemunicipio" = "character", 
+      "vacina_dataAplicacao" = "character", 
+      "paciente_endereco_coIbgeMunicipio" = "character", 
       "vacina_descricao_dose" = "character"), 
     col.names = c("date", "ibge", "type"), 
     encoding = "UTF-8", 
@@ -55,6 +65,11 @@ x <-
   dplyr::mutate(date = format(date, "%Y-%m-%d")) %>%
   # capitalize columns
   dplyr::rename(Date = date, IBGE6 = ibge, Type = type, N = n)
+  
+})
+
+# Merge data
+x <- data.table::rbindlist(x)
 
 # Write output file in the download folder
 data.table::fwrite(x, file = sprintf("download/%s.csv.gz", state), row.names = FALSE)
